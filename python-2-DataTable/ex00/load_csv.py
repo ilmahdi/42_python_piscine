2c1d2887f4e3d079ed18adabab25debccfe34032
El mahdi Abdelhadi <eabdelha@e2r8p1.1337.ma>
2025-01-16 09:26:41 +0100

    day 02 ex00

---
+++ python-2-DataTable/ex00/load_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """
+    Load a dataset from a CSV file into a pandas DataFrame.
+
+    Args:
+        path (str): The file path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The loaded dataset as a pandas DataFrame.
+    """
+    df = pd.read_csv(path)
+    print(f"Dataset dimensions: {df.shape}")
+    return df
